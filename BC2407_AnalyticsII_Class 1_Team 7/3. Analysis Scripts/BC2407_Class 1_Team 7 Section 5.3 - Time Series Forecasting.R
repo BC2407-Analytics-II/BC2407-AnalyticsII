f195bab5ecbb7b6448c2cf948509a16afe5fcff9
Randomystick <bryankhleow@gmail.com>
2022-04-04 02:55:06 +0800

    updated forecasts

--- BC2407_AnalyticsII_Class 1_Team 7/3. Analysis Scripts/BC2407_Class 1_Team 7 Section 5.3 - Time Series Forecasting.R
+++ BC2407_AnalyticsII_Class 1_Team 7/3. Analysis Scripts/BC2407_Class 1_Team 7 Section 5.3 - Time Series Forecasting.R
@@ -465,7 +465,6 @@ arima.3.auto = accuracy(m.arima.forecasts, testset3)
 arima.3.auto
 #######                                          END                                          #######
 
-
 dfTS.forecaster <- arima(dfTS.ts, order=c(0,0,1))
 dfTS.forecasts <- forecast(dfTS.forecaster, h = 12)
 write.csv(fitted(dfTS.forecaster), 'dfTS_fitted.csv')
@@ -476,9 +475,6 @@ df2TS.forecasts <- forecast(df2TS.forecaster, h = 12)
 write.csv(fitted(df2TS.forecaster), 'df2TS_fitted.csv')
 write.csv(df2TS.forecasts, 'df2TS_forecasts.csv')
 
-
-df3TS.forecaster <- arima(df3TS.ts, order=c(1,0,1))
-df3TS.forecasts <- forecast(df3TS.forecaster, h = 12)
-write.csv(fitted(df3TS.forecaster), 'df3TS_fitted.csv')
-write.csv(dfTS.forecasts, 'df3TS_forecasts.csv')
+write.csv(fitted(m.arima.forecasts), 'df3TS_fitted.csv')
+write.csv(m.arima.forecasts, 'df3TS_forecasts.csv')
 
